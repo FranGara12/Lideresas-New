d9810b3d82c0d3e60e4b61cfc4a4b4600d656424
sistemas <sistemas@SPO_LAB50705.inacap.cl>
2025-12-03 12:21:14 -0300

    pruebas vercel

--- lideresas/settings.py
+++ lideresas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0tgb5mg#$xx5pnq49nt&#su0^h164hqoeh)&vol2ch4mdrq$1f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app', 'localhost']
 
 
 # Application definition
