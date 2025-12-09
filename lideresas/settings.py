47ca8bdf57162c37eb2dee2750daf0763d55063e
Fran Gara <franciscagg1112@gmail.com>
2025-12-09 20:49:10 -0300

    fix vercel dependencies

--- lideresas/settings.py
+++ lideresas/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-0tgb5mg#$xx5pnq49nt&#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app', 'localhost', '*.vercel.app']
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
---
+++ vercel_app.py
@@ -0,0 +1,4 @@
+import os
+from lideresas.wsgi import application
+
+app = application
