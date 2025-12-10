61afa0101cfaf1137af661dae58f882356b25948
Fran Gara <franciscagg1112@gmail.com>
2025-12-09 21:08:51 -0300

    fix statix files

--- lideresas/settings.py
+++ lideresas/settings.py
@@ -121,8 +121,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Whitenoise para servir archivos estáticos
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
